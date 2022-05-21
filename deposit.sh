8148ec98234384473d9386e90d890e2651f3abf9
vasrox <77592096+vasrox@users.noreply.github.com>
2022-05-21 12:15:54 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-for ((i = 0 ; i < 200 ; i++)); do
-    echo $i
-    echo ""
-    ironfish deposit --confirm -v
-    sleep $[ (180 + $RANDOM % 10 ) ]s
-    echo ""
+dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi
+while true; do
+BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }')
+echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
+if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
+ echo -e '\033[1;32m'"Transaction:"'\033[0m'
+ /usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm
+ echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
+else
+ sleep $[ (120 + $RANDOM % 10 ) ]s
+fi
 done
